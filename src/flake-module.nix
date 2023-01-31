dd847d3b879f30ec60c849098c13d91688dd3310
Jörg Thalheim <joerg@thalheim.io>
2023-01-31 09:33:56 +0100

    wip: pxe boot

--- src/flake-module.nix
+++ src/flake-module.nix
@@ -5,7 +5,14 @@
       default = config.packages.nixos-remote;
     };
     devShells.default = pkgs.mkShellNoCC {
-      packages = config.packages.nixos-remote.runtimeInputs;
+      packages = [
+        pkgs.mypy
+        pkgs.pixiecore
+        pkgs.dnsmasq
+        pkgs.python3.pkgs.netaddr
+        pkgs.qemu_kvm
+      ] ++ config.packages.nixos-remote.runtimeInputs;
+      OVMF = "${pkgs.OVMF.fd}/FV/OVMF.fd";
     };
   };
 }
